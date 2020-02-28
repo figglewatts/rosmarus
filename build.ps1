87367198cf1c7cee430ade85599dfcb109d701ad
Figglewatts <me@figglewatts.co.uk>
2020-02-28 14:54:20 +0000

    Added openAL to vendor deps

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 rm -r dist
 rm -r build
-pipenv run pyinstaller --paths $(pipenv --venv) --add-binary "vendor/*;." --add-data "Rosmarus_test_data;Rosmarus_test_data" --hidden-import='pkg_resources.py2_warn' --clean --onefile --noconsole .\functionality.py
+pipenv run pyinstaller --paths $(pipenv --venv) --add-binary "vendor/*;." --add-data "Rosmarus_test_data;Rosmarus_test_data" --hidden-import='pkg_resources.py2_warn' --clean --onefile --no-console .\functionality.py
